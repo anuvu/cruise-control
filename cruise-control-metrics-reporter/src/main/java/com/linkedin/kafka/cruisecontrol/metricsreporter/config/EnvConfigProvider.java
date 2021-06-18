f35973c725b97a6de964a2c28a01ca3e69716ba5
mavemuri <maruthi.vemuri1@gmail.com>
2021-06-17 18:01:53 -0700

    fix environment variables in config not being set

--- cruise-control-metrics-reporter/src/main/java/com/linkedin/kafka/cruisecontrol/metricsreporter/config/EnvConfigProvider.java
+++ cruise-control-metrics-reporter/src/main/java/com/linkedin/kafka/cruisecontrol/metricsreporter/config/EnvConfigProvider.java
@@ -19,6 +19,12 @@ public class EnvConfigProvider implements ConfigProvider {
 
   private Map<String, String> _preConfiguredEnvironmentVariables;
 
+  private static Map<String, String> _systemVariables;
+
+  static {
+    _systemVariables = System.getenv();
+  }
+
   @Override
   public ConfigData get(String path) {
     assertNoPath(path);
@@ -45,6 +51,7 @@ public class EnvConfigProvider implements ConfigProvider {
     _preConfiguredEnvironmentVariables = configs.entrySet()
       .stream()
       .collect(Collectors.toMap(Entry::getKey, kv -> (String) kv.getValue()));
+    _preConfiguredEnvironmentVariables.putAll(_systemVariables);
   }
 
   private static void assertNoPath(String path) {
