0cc83e4f49d8f7f7af38defb65080fa72a84e716
Maruthi Vemuri <mavemuri@cisco.com>
2024-05-07 23:05:47 +0000

    fix clusterModel

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/monitor/LoadMonitor.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/monitor/LoadMonitor.java
@@ -501,7 +501,7 @@ public class LoadMonitor {
     // Create an empty cluster model first.
     long currentLoadGeneration = partitionMetricSampleAggregationResult.generation();
     ModelGeneration modelGeneration = new ModelGeneration(clusterAndGeneration.generation(), currentLoadGeneration);
-    ClusterModel clusterModel = new ClusterModel(modelGeneration, partitionMetricSampleAggregationResult.validEntityRatioOfCompleteness());
+    ClusterModel clusterModel = new ClusterModel(modelGeneration, partitionMetricSampleAggregationResult.validEntityRatioOfCompleteness(), cluster);
     
     // ClusterModel clusterModel = new ClusterModel(modelGeneration, partitionMetricSampleAggregationResult.completeness().validEntityRatio(), 
     // cluster);
@@ -545,7 +545,7 @@ public class LoadMonitor {
 
     // Create an empty cluster model first.
     ModelGeneration modelGeneration = new ModelGeneration(clusterAndGeneration.generation(), -1L);
-    ClusterModel clusterModel = new ClusterModel(modelGeneration, 0.0);
+    ClusterModel clusterModel = new ClusterModel(modelGeneration, 0.0, cluster);
 
     populateClusterCapacity(false, false, clusterModel, cluster);
     // Set the state of bad brokers in clusterModel based on the Kafka cluster state.
